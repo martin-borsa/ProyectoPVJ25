eaf8d3d4e91449fb5262d2b3a548e23e0dd6e348
GitFacu <hanfacundosolo@gmail.com>
2025-06-23 13:55:33 -0300

    Menu y creditos

---
+++ Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MenuInicial");
+    }
+
+    //public void Controles()
+    //{
+    //    SceneManager.LoadScene("Controles"); //Este lo acabo de poner
+    //}
+
+    //public void Exit()
+    //{
+    //    Application.Quit();
+    //}
+}
